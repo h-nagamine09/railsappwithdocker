49aa95347999b5b2be699c1ca66216693c7d59d8
Hiroki Nagamine <hiroki.nagamine@gmail.com>
2019-10-08 22:34:17 +0900

    掲示板機能の作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,29 @@
 class BoardsController < ApplicationController
  def index
+   @boards = Board.all
+ end
+
+ def show
+   @board = Board.find(params[:id])
  end
 
  def new
    @board = Board.new
+ end
+
+ def create
+   Board.create(board_params)
+ end
+
+ def updated
+   board = Board.find(params[:id])
+   board.update(board_params)
+   redirect_to board
+ end
+
+ private
+
+ def board_params
+   params.require(:board).permit(:name, :title, :body)
  end
 end
