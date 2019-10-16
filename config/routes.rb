c0caa386ec3d9bbbfc20df1d79971eaa35a41f1e
Hiroki Nagamine <hiroki.nagamine@gmail.com>
2019-10-16 21:39:41 +0900

    rspecを使ったテストコードを記述

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   get 'sessions/create'
-
   get 'sessions/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
